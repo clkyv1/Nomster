866f4709043519f0307740b842b92edaddc361c6
Brian Randall <brian.lewis.randall@outlook.com>
2019-08-29 02:26:41 +0000

    Brian

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,12 +23,7 @@ class PlacesController < ApplicationController
     @comment = Comment.new
   end
 
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+    
 
   def edit
     @place = Place.find(params[:id])
@@ -42,16 +37,14 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+
+  :forbidden
     end
+
 
     @place.update_attributes(place_params)
     redirect_to root_path
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
-
+    
   end
 
   def destroy

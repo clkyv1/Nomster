62ab5468c7a04e25e6de640daf6bb10814cc216c
Brian Randall <brian.lewis.randall@outlook.com>
2019-09-06 03:34:20 +0000

    brian

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,8 +2,7 @@ class PhotosController < ApplicationController
 before_action :authenticate_user!
   
   def create
-     @place = Place.find(params[:place_id,:picture])
-
+     @place = Place.find(params[:place_id])
      @photo = photos.create(photo_params.merge(user: current_user))
      redirect_to place_path(@place)
   end
